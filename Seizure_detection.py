6f87fb46ad5331a7d95dff4d71d69efeb04db8f9
Ammar-Elsaeed <ammar.ibrahim00@eng-st.cu.edu.eg>
2022-02-08 00:32:22 +0200

    fixed wilcoxon test

--- Seizure_detection.py
+++ Seizure_detection.py
@@ -1,20 +1,22 @@
 import numpy as np
 import pandas as pd
+from numpy.fft import fft, fftfreq , rfft, rfftfreq
 from sklearn.model_selection import KFold
-import pyeeg
-from eeglib import features
-import pywt  # py wavelets pywt.wavedec()
-from sklearn import svm
-# for mutual info
-from sklearn.feature_selection import mutual_info_classif, SelectKBest, SelectPercentile
+# import pyeeg 
+# from eeglib import features
+import pywt #py wavelets pywt.wavedec()
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import mutual_info_classif,SelectKBest, SelectPercentile #for mutual info
 from sklearn.decomposition import PCA
-from scipy.stats import ranksums  # for wilcoxon function
+from scipy.stats import ranksums # for wilcoxon function
 from scipy import signal as sig
-from scipy.stats import skew, kurtosis
+from scipy.stats import skew,kurtosis
+from sklearn.model_selection import cross_validate
 eeg_sampling = 256
 # Preprocessing
-
-
 def apply_filter(signal, low_freq=None, high_freq=None):
     # apply a bandpass filter to the signal where a and b are the corner frequencies
     low_freq = low_freq/eeg_sampling
@@ -35,7 +37,14 @@ def filter_bank(signal, cutoffs=None):
             signal, low_freq=bands[0], high_freq=bands[1])
     return filtered_signals
 
-
+def split_data(X,Y,split_ratio = 0.2,normalization=True):
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=split_ratio, random_state=1)
+    if normalization:
+        Scaler_tr = StandardScaler().fit(X_train)
+        X_train = Scaler_tr.transform(X_train)
+        X_test = Scaler_tr.transform(X_test)
+    
+    return X_train, X_test, Y_train, Y_test
 def normalization(train_features, test_features=None):
     # mean and min-max normalization
     norm_train_features = np.empty(
@@ -82,33 +91,53 @@ def subset_trials(signal, trial_length):
     # Feature extraction
 
 
-def freq_features():
-    # Compute frequency features for signal (from paper)
-    return frequency_features
+def peak_frequency(signal):
+    peak_freq = []
+    ft = fft(signal)
+    mag = np.abs(ft)
+    freq = fftfreq(mag.size,1/256)
+    
+    cutoffs = [0.5,4,8,12,25]
+    for i in range(len(cutoffs)-1):
+        cut_freq = freq[np.logical_and(freq>cutoffs[i],freq<cutoffs[i+1])]
+        cut_mag = mag[np.logical_and(freq>cutoffs[i],freq<cutoffs[i+1])]
+        peak_freq.append(cut_freq[cut_mag==max(cut_mag)][0])
 
+    return peak_freq
 
-def stat_features(signal):
-    # compute mean skewness variance and kurtosis on time domain signal and wavelet coefficients
-    statistical_features = []
+def extract_features(signal):
+    peak_freq = []
+    rms = []
+    entropy = []
+    stat_features = []
     for trial in range(signal.shape[0]):
-        electrodes_features = []
+        trial_stat = []
+        trial_freq = []
+        trial_rms = []
+        trial_entropy = []
+        
         for electrode in range(signal.shape[2]):
-            electrodes_features.append(np.mean(signal[trial, :, electrode]))
-            electrodes_features.append(np.var(signal[trial, :, electrode]))
-            electrodes_features.append(skew(signal[trial, :, electrode]))
-            electrodes_features.append(kurtosis(signal[trial, :, electrode]))
+            trial_rms.append(np.sqrt(np.mean(signal[trial,:,electrode]**2)))
+            trial_entropy.append(np.sum(signal[trial,:,electrode]*np.log(signal[trial,:,electrode]**2)))
+            trial_freq += peak_frequency(signal[trial,:,electrode])
+            trial_stat.append(np.mean(signal[trial,:,electrode]))
+            trial_stat.append(np.var(signal[trial,:,electrode]))
+            trial_stat.append(skew(signal[trial,:,electrode]))
+            trial_stat.append(kurtosis(signal[trial,:,electrode]))
 
-            coeff = pywt.wavedec(signal[trial, :, electrode], 'db4', level=5)
+            coeff = pywt.wavedec(signal[trial,:,electrode],'db4',level=5)
             for level in range(len(coeff)):
-                electrodes_features.append(np.mean(coeff[level], axis=0))
-                electrodes_features.append(np.var(coeff[level], axis=0))
-                electrodes_features.append(skew(coeff[level], axis=0))
-                electrodes_features.append(kurtosis(coeff[level], axis=0))
-
-        statistical_features.append(electrodes_features)
-    statistical_features = np.asarray(statistical_features)
-    # 136 * 644. 644: 23 electrode * 28 feature. 28 feature: 4 time, 6*4 wavelets
-    return statistical_features
+                trial_stat.append(np.mean(coeff[level],axis =0))
+                trial_stat.append(np.var(coeff[level],axis =0))
+                trial_stat.append(skew(coeff[level],axis =0))
+                trial_stat.append(kurtosis(coeff[level],axis =0))
+        peak_freq.append(trial_freq)
+        rms.append(trial_rms)
+        entropy.append(trial_entropy)
+        stat_features.append(trial_stat)
+    
+    features = np.concatenate((peak_freq,rms,entropy,stat_features),axis=1)
+    return features
 
 
 def pypackage_features():
@@ -127,16 +156,23 @@ def mutual_info(features, cdf):
     return selected_features
 
 
-def wilcoxon_test(features, p_value):
+def wilcoxon_test(X_train,X_test,X_cv, Y_train,threshold):
     # use wilcoxon test as a criteria to select the best features, and return them
     selected_features = []
-    for i in range(len(features) - 1):
-        for j in range(i + 1, len(features) - 1):
-            rank = ranksums(features[i], features[j])
-            if (rank.pvalue <= p_value):
-                pass  # did not fully understand what should be done here XD
-    return selected_features
-
+    
+    class_1 = X_train[np.where(Y_train==0)]
+    class_2 = X_train[np.where(Y_train==1)]
+    p_Values = []
+    for i in range(X_train.shape[1]):
+        feature_class_1 = class_1[:,i]
+        feature_class_2 = class_2[:,i]
+        _,p_Value = ranksums(feature_class_1,feature_class_2)
+        p_Values.append(p_Value)
+    p_Values = np.asarray(p_Values)
+    X_train_reduced = np.squeeze(X_train[:,np.where(p_Values<threshold)])
+    X_test_reduced = np.squeeze(X_test[:,np.where(p_Values<threshold)])
+    X_cv_reduced = np.squeeze(X_cv[:,np.where(p_Values<threshold)])
+    return X_train_reduced,X_test_reduced,X_cv_reduced
 
 def pca(features, variance):
     # use PCA for feature reduction with a specified variance to keep.
@@ -165,9 +201,21 @@ subsetted_ictal, labels_ictal = subset_trials(ictal_data, 1)
 all_trials = np.concatenate((subsetted_preictal, subsetted_ictal), axis=0)
 # Zero is preictal, 1 is ictal
 all_labels = np.concatenate((labels_preictal, labels_ictal), axis=0)
-stat = stat_features(all_trials)
-filtered_signals = filter_bank(all_trials)
-kfold = KFold(n_splits=10, shuffle=True, random_state=42)
+
+X_train, X_test, Y_train, Y_test = split_data(extract_features(all_trials),all_labels, 0.4)
+trainer = SVC(kernel='sigmoid')
+trainer.fit(X_train,Y_train)
+print(accuracy_score(Y_test,trainer.predict(X_test)))
+# -------------------------
+X_test, X_cv, y_test, y_cv = split_data(X_test, Y_test, 0.5, False)
+
+# X_train_reduced, X_test_reduced, X_cv_reduced = mutual_info(X_train, X_test, X_cv, Y_train, 10)
+X_train_reduced, X_test_reduced, X_cv_reduced = wilcoxon_test(X_train, X_test, X_cv, Y_train,0.005)
+# X_train_reduced, X_test_reduced, X_cv_reduced = pca(X_train, X_test, X_cv, 20)
+
+trainer = SVC(kernel='sigmoid')
+trainer.fit(X_train_reduced,Y_train)
+print(accuracy_score(y_cv,trainer.predict(X_cv_reduced)), end=' ')
 # for train_Idx, test_Idx in kfold.split(all_labels):
 # call preprocessing
 # call feature extraction
